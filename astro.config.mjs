6cf3b9afbcff08df00f4348a85789e2d8eef496a
LeeSin77 <slee82764@gmail.com>
2025-12-16 23:59:54 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://leesin77.github.io",
+	base: "/my-blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
